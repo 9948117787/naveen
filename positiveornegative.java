f89e16d019382bc1039b025473093a07bd5eb2fd
9948117787 <42460082+9948117787@users.noreply.github.com>
2018-08-17 11:24:51 +0530

    Update and rename positive or negative to positiveornegative.java

--- positiveornegative.java
+++ positiveornegative.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class positive or negative
+class positiveornegative
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
