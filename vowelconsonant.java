57398433bd1bbc2c473dee4da9190c21c5bbdc3d
9948117787 <42460082+9948117787@users.noreply.github.com>
2018-08-20 11:56:30 +0530

    Create vowelconsonant.java

---
+++ vowelconsonant.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class vowelconsonant
+{
+    public static void main(String[] args)
+    {
+    	Scanner s=new Scanner (System.in);
+
+        char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
