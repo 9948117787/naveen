dcad5aa2c0d4f9c2b54a856da2dbaaabafc7799b
9948117787 <42460082+9948117787@users.noreply.github.com>
2018-08-20 12:33:08 +0530

    Create alphabetornot.java

---
+++ alphabetornot.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class alphbetornot
+{ 
+	public static void main(String[] args)
+	{
+             Scanner s=new Scanner(System.in);
+        char c = '*';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
+	
