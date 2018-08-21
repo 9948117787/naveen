d79054364e7efbde65bd5d3c0dc5a56cedcca934
9948117787 <42460082+9948117787@users.noreply.github.com>
2018-08-21 12:40:41 +0530

    Update largest.java

--- largest.java
+++ largest.java
@@ -6,10 +6,10 @@ public class Largest
 {
     public static void main(String[] args) {
     	Scanner s=new Scanner(System.in);
-    	double n1,n2,n3;
-        n1=s.nextDouble();
-        n2=s.nextDouble();
-        n3=s.nextDouble();
+    	int n1,n2,n3;
+        n1=s.nextInt();
+        n2=s.nextInt();
+        n3=s.nextInt();
         if( n1 >= n2 && n1 >= n3)
             System.out.println(n1);
 
