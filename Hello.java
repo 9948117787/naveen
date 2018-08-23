683665e291a8f5a5db840cc144b6de0e3758109d
9948117787 <42460082+9948117787@users.noreply.github.com>
2018-08-23 11:04:11 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=0;
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(i=0;i<n;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
