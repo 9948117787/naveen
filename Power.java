a0eada8ed393b112e3dfa7a85630ed8a1773225b
9948117787 <42460082+9948117787@users.noreply.github.com>
2018-08-28 12:10:04 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Power
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.print((int)Math.pow(a,b));
+	}
+}
