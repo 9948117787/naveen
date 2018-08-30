38895ecff8950a7d07b2e3292891a2b449b86123
9948117787 <42460082+9948117787@users.noreply.github.com>
2018-08-30 13:01:24 +0530

    Create Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Multiples
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=5;i++)
+		System.out.print(n*i+" ");
+	}
+}
