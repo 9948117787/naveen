3cf968c09d7dfe68728a872949063bf452233bfe
9948117787 <42460082+9948117787@users.noreply.github.com>
2018-08-30 11:35:28 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Even
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n1=s.nextInt();
+		int n2=s.nextInt();
+		for (int i = n1+1; i <=n2; i++)
+		{
+			if (i % 2 == 0)
+			{
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
+
+		
