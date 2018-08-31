eafbb6f6f805f6fe7d1e30c44e1e91bd9b1ca493
9948117787 <42460082+9948117787@users.noreply.github.com>
2018-08-31 14:37:52 +0530

    Create ArraysMax.java

---
+++ ArraysMax.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays.*;
+public class ArraysMax
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		Arrays.sort(a);
+		System.out.print(a[a.length-1]);
+ 
+	}
+}
